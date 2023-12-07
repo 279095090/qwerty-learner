fffcf322b1d0ce6b8551dcec3f7272c5aa69e0af
fanerstar <fanerstar@gmail.com>
2023-12-07 21:34:24 +0800

    update my setting ,type - to play sounds

--- src/pages/ErrorBook/RowDetail/index.tsx
+++ src/pages/ErrorBook/RowDetail/index.tsx
@@ -57,7 +57,7 @@ const RowDetail: React.FC<RowDetailProps> = ({ currentRowDetail, allRecords }) =
   )
 
   useHotkeys(
-    'ctrl+j',
+    '-',
     () => {
       wordPronunciationIconRef.current?.play()
     },
